b5a15d4b6a6abb088c4f89c4dec5cbe495a9b6ff
Baptiste Langlade <baptouuuu@gmail.com>
2017-06-25 12:10:54 +0200

    add action history

---
+++ tests/ActionHistory/FilesystemTest.php
@@ -0,0 +1,167 @@
+<?php
+declare(strict_types = 1);
+
+namespace Tests\Innmind\Homeostasis\ActionHistory;
+
+use Innmind\Homeostasis\{
+    ActionHistory\Filesystem,
+    ActionHistory,
+    Action,
+    Strategy,
+    TimeContinuum\Format\ISO8601WithMilliseconds
+};
+use Innmind\Filesystem\{
+    AdapterInterface,
+    File,
+    FileInterface,
+    Stream\StringStream
+};
+use Innmind\TimeContinuum\{
+    TimeContinuumInterface,
+    TimeContinuum\Earth
+};
+use Innmind\Math\Algebra\Number\Number;
+use Innmind\Immutable\{
+    Map,
+    StreamInterface
+};
+use PHPUnit\Framework\TestCase;
+
+class FilesystemTest extends TestCase
+{
+    public function testInterface()
+    {
+        $this->assertInstanceOf(
+            ActionHistory::class,
+            new Filesystem(
+                $this->createMock(AdapterInterface::class),
+                $this->createMock(TimeContinuumInterface::class)
+            )
+        );
+    }
+
+    public function testAdd()
+    {
+        $history = new Filesystem(
+            $filesystem = $this->createMock(AdapterInterface::class),
+            $clock = new Earth
+        );
+        $now = $clock->now();
+        $action = new Action(
+            $now,
+            Strategy::holdSteady()
+        );
+        $now = $now->format(new ISO8601WithMilliseconds);
+        $filesystem
+            ->expects($this->once())
+            ->method('add')
+            ->with($this->callback(function(File $file) use ($now): bool {
+                return (string) $file->name() === md5($now) &&
+                    (string) $file->content() === json_encode([
+                        'time' => $now,
+                        'strategy' => 'holdSteady',
+                    ]);
+            }));
+
+        $this->assertSame($history, $history->add($action));
+    }
+
+    public function testAll()
+    {
+        $history = new Filesystem(
+            $filesystem = $this->createMock(AdapterInterface::class),
+            $clock = new Earth
+        );
+        $now = $clock->now()->format(new ISO8601WithMilliseconds);
+        usleep(100);
+        $now2 = $clock->now()->format(new ISO8601WithMilliseconds);
+        $filesystem
+            ->expects($this->once())
+            ->method('all')
+            ->willReturn(
+                (new Map('string', FileInterface::class))
+                    ->put(
+                        md5('foo'),
+                        new File(
+                            md5('foo'),
+                            new StringStream(json_encode([
+                                'time' => $now,
+                                'strategy' => 'holdSteady',
+                            ]))
+                        )
+                    )
+                    ->put(
+                        md5('bar'),
+                        new File(
+                            md5('bar'),
+                            new StringStream(json_encode([
+                                'time' => $now2,
+                                'strategy' => 'increase',
+                            ]))
+                        )
+                    )
+        );
+
+        $actions = $history->all();
+
+        $this->assertInstanceOf(StreamInterface::class, $actions);
+        $this->assertSame(Action::class, (string) $actions->type());
+        $this->assertCount(2, $actions);
+        $action = $actions->current();
+        $this->assertInstanceOf(Action::class, $action);
+        $now = $clock->at($now);
+        $this->assertTrue($action->time()->equals($now));
+        $this->assertSame(Strategy::holdSteady(), $action->strategy());
+        $actions->next();
+        $action = $actions->current();
+        $this->assertInstanceOf(Action::class, $action);
+        $now2 = $clock->at($now2);
+        $this->assertTrue($action->time()->equals($now2));
+        $this->assertSame(Strategy::increase(), $action->strategy());
+    }
+
+    public function testKeepUp()
+    {
+        $history = new Filesystem(
+            $filesystem = $this->createMock(AdapterInterface::class),
+            $clock = new Earth
+        );
+        $now = $clock->now()->format(new ISO8601WithMilliseconds);
+        sleep(1);
+        $mark = $clock->now();
+        sleep(1);
+        $now2 = $clock->now()->format(new ISO8601WithMilliseconds);
+        $filesystem
+            ->expects($this->once())
+            ->method('all')
+            ->willReturn(
+                (new Map('string', FileInterface::class))
+                    ->put(
+                        md5('foo'),
+                        new File(
+                            md5('foo'),
+                            new StringStream(json_encode([
+                                'time' => $now,
+                                'strategy' => 'increase',
+                            ]))
+                        )
+                    )
+                    ->put(
+                        md5('bar'),
+                        new File(
+                            md5('bar'),
+                            new StringStream(json_encode([
+                                'time' => $now2,
+                                'strategy' => 'decrease',
+                            ]))
+                        )
+                    )
+        );
+        $filesystem
+            ->expects($this->once())
+            ->method('remove')
+            ->with(md5('foo'));
+
+        $this->assertSame($history, $history->keepUp($mark));
+    }
+}
---
+++ tests/ActionTest.php
@@ -0,0 +1,76 @@
+<?php
+declare(strict_types = 1);
+
+namespace Tests\Innmind\Homeostasis;
+
+use Innmind\Homeostasis\{
+    Action,
+    Strategy
+};
+use Innmind\TimeContinuum\PointInTimeInterface;
+use Innmind\Math\Algebra\Integer;
+use PHPUnit\Framework\TestCase;
+
+class ActionTest extends TestCase
+{
+    public function testInterface()
+    {
+        $action = new Action(
+            $time = $this->createMock(PointInTimeInterface::class),
+            Strategy::holdSteady()
+        );
+
+        $this->assertSame($time, $action->time());
+        $this->assertSame(Strategy::holdSteady(), $action->strategy());
+    }
+
+    /**
+     * @dataProvider cases
+     */
+    public function testVariation($current, $previous, $expected)
+    {
+        $variation = (new Action(
+            $this->createMock(PointInTimeInterface::class),
+            Strategy::{$current}()
+        ))->variation(
+            new Action(
+                $this->createMock(PointInTimeInterface::class),
+                Strategy::{$previous}()
+            )
+        );
+
+        $this->assertInstanceOf(Integer::class, $variation);
+        $this->assertSame($expected, $variation->value());
+    }
+
+    public function cases(): array
+    {
+        return [
+            ['dramaticDecrease', 'dramaticDecrease', 0],
+            ['dramaticDecrease', 'decrease', 1],
+            ['dramaticDecrease', 'holdSteady', 1],
+            ['dramaticDecrease', 'increase', 1],
+            ['dramaticDecrease', 'dramaticIncrease', 1],
+            ['decrease', 'dramaticDecrease', -1],
+            ['decrease', 'decrease', 0],
+            ['decrease', 'holdSteady', 0],
+            ['decrease', 'increase', 0],
+            ['decrease', 'dramaticIncrease', 1],
+            ['holdSteady', 'dramaticDecrease', -1],
+            ['holdSteady', 'decrease', 0],
+            ['holdSteady', 'holdSteady', 0],
+            ['holdSteady', 'increase', 0],
+            ['holdSteady', 'dramaticIncrease', 1],
+            ['increase', 'dramaticDecrease', -1],
+            ['increase', 'decrease', 0],
+            ['increase', 'holdSteady', 0],
+            ['increase', 'increase', 0],
+            ['increase', 'dramaticIncrease', 1],
+            ['dramaticIncrease', 'dramaticDecrease', -1],
+            ['dramaticIncrease', 'decrease', -1],
+            ['dramaticIncrease', 'holdSteady', -1],
+            ['dramaticIncrease', 'increase', -1],
+            ['dramaticIncrease', 'dramaticIncrease', 0],
+        ];
+    }
+}
