4b914198525008220a077dfd8df6990007cf6bea
Baptiste Langlade <baptouuuu@gmail.com>
2017-12-10 13:38:19 +0100

    throw an exception when cannot determine a polynom for the given dataset

--- src/Math/PolynomialRegression/BestFit.php
+++ src/Math/PolynomialRegression/BestFit.php
@@ -3,11 +3,13 @@ declare(strict_types = 1);
 
 namespace Innmind\Homeostasis\Math\PolynomialRegression;
 
+use Innmind\Homeostasis\Exception\BestFitNotDeterminable;
 use Innmind\Math\{
     Regression\Dataset,
     Regression\PolynomialRegression,
     Algebra\Integer,
-    Polynom\Polynom
+    Polynom\Polynom,
+    Exception\Exception
 };
 
 final class BestFit
@@ -22,18 +24,26 @@ final class BestFit
     public function __invoke(Integer $a, Integer $b): Polynom
     {
         do {
-            $regressed = new PolynomialRegression($this->dataset, $a);
+            try {
+                $regressed = new PolynomialRegression($this->dataset, $a);
 
-            if (!isset($bestFit)) {
-                $bestFit = $regressed;
-            }
+                if (!isset($bestFit)) {
+                    $bestFit = $regressed;
+                }
 
-            if ($bestFit->rootMeanSquareDeviation()->higherThan($regressed->rootMeanSquareDeviation())) {
-                $bestFit = $regressed;
+                if ($bestFit->rootMeanSquareDeviation()->higherThan($regressed->rootMeanSquareDeviation())) {
+                    $bestFit = $regressed;
+                }
+            } catch (Exception $e) {
+                //attempt higher degree
             }
 
             $a = $a->increment();
         } while ($b->higherThan($a) || $b->equals($a));
+
+        if (!isset($bestFit)) {
+            throw new BestFitNotDeterminable;
+        }
 
         return $bestFit->polynom();
     }
